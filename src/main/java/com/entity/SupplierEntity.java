0dc2a15b7651ab06152681db0e4cd529d56f0f5b
VijayPatil86 <vijipatil090386@gmail.com>
2024-08-25 20:44:54 +0530

    initial commit

---
+++ src/main/java/com/entity/SupplierEntity.java
@@ -0,0 +1,28 @@
+package com.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter @Setter @ToString @NoArgsConstructor
+@Entity
+@Table(name = "SUPPLIER_MASTER")
+public class SupplierEntity {
+	@Column(name = "supplier_id")
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int supplierId;
+
+	@Column(name = "supplier_name")
+	private String supplierName;
+
+	@Column(name="supplier_city")
+	private String supplierCity;
+}
