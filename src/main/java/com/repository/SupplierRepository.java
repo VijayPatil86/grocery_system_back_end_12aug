ac1c604a6cfc1385168fac233cc9c9663af2f98c
VijayPatil86 <vijipatil090386@gmail.com>
2024-08-26 08:45:01 +0530

    added custom method

--- src/main/java/com/repository/SupplierRepository.java
+++ src/main/java/com/repository/SupplierRepository.java
@@ -1,9 +1,11 @@
 package com.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.entity.SupplierEntity;
 
 public interface SupplierRepository extends JpaRepository<SupplierEntity, Integer> {
-
+	Optional<Boolean> existsBySupplierNameAndSupplierCity(String supplierName, String supplierCity);
 }
