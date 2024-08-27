e6a295c5b14363777340ff022461c5ff4bab4c83
VijayPatil86 <vijipatil090386@gmail.com>
2024-08-27 23:14:46 +0530

    logger related changes

--- src/main/java/com/controller/SupplierController.java
+++ src/main/java/com/controller/SupplierController.java
@@ -2,6 +2,8 @@ package com.controller;
 
 import java.util.List;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,30 +23,45 @@ import com.service.SupplierService;
 @RequestMapping("/api")
 public class SupplierController {
 
+	private static final Logger logger = LogManager.getLogger(SupplierController.class);
+
 	@Autowired
 	private SupplierService supplierService;
 
 	@GetMapping("/suppliers")
 	public ResponseEntity<List<SupplierBean>> getAllSuppliers() {
+		logger.debug("GET /api/suppliers - getAllSuppliers() - START");
 		List<SupplierBean> allSuppliers = supplierService.getAllSuppliers();
-		if(allSuppliers.size() == 0)
+		logger.debug("GET /api/suppliers - getAllSuppliers() - allSuppliers.size " + allSuppliers.size());
+		if(allSuppliers.size() == 0) {
+			logger.debug("GET /api/suppliers - getAllSuppliers() - allSuppliers.size is 0, throwing SuppliersNotFoundException");
 			throw new SuppliersNotFoundException();
+		}
+		logger.debug("GET /api/suppliers - getAllSuppliers() - END");
+		logger.debug("GET /api/suppliers - getAllSuppliers() - Returning 200 OK");
 		return ResponseEntity.ok().body(allSuppliers);
 	}
 
 	@PostMapping(value = "/suppliers")
 	public ResponseEntity<String> registerSupplier(@RequestBody SupplierBean supplierBean) {
+		logger.debug("POST /api/suppliers - registerSupplier() - START");
+		logger.debug("POST /api/suppliers - registerSupplier() - Supplier to Register: " + supplierBean);
 		supplierService.registerSupplier(supplierBean);
+		logger.info("POST /api/suppliers - registerSupplier() - Supplier Registered");
+		logger.debug("POST /api/suppliers - registerSupplier() - END");
+		logger.debug("POST /api/suppliers - registerSupplier() - Returning 200 OK");
 		return ResponseEntity.ok().body("Supplier registered");
 	}
 
 	@ExceptionHandler(value = {SuppliersNotFoundException.class})
 	public ResponseEntity<Object> handleSuppliersNotFoundException() {
+		logger.debug("SuppliersNotFoundException - Returning 204 No Content");
 		return ResponseEntity.noContent().build();
 	}
 
 	@ExceptionHandler(value = {SupplierExistsException.class})
 	public ResponseEntity<String> handleSupplierExistsException(SupplierExistsException supplierExistsException) {
+		logger.debug("SupplierExistsException - Returning 409 Conflict");
 		return new ResponseEntity<>(supplierExistsException.getMessage(), HttpStatus.CONFLICT);
 	}
 }
--- src/main/java/com/service/SupplierService.java
+++ src/main/java/com/service/SupplierService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,29 +17,44 @@ import com.repository.SupplierRepository;
 @Service
 public class SupplierService {
 
+	private static final Logger logger = LogManager.getLogger(SupplierService.class);
+
 	@Autowired
 	private SupplierRepository supplierRepository;
 
 	public List<SupplierBean> getAllSuppliers() {
-		List<SupplierEntity> allSuppliers = supplierRepository.findAll();
-		return allSuppliers.stream().map(
+		logger.debug("getAllSuppliers() - START");
+		List<SupplierEntity> allSuppliersEntities = supplierRepository.findAll();
+		logger.debug("getAllSuppliers() - allSuppliersEntities size " + allSuppliersEntities.size());
+		List<SupplierBean> allSuppliersBeans = allSuppliersEntities.stream().map(
 				supplierEntity -> SupplierBean.builder()
 									.supplierId(supplierEntity.getSupplierId())
 									.supplierName(supplierEntity.getSupplierName())
 									.supplierCity(supplierEntity.getSupplierCity())
 									.build()
 			).collect(Collectors.toList());
+		logger.debug("getAllSuppliers() - allSuppliersBeans size " + allSuppliersBeans.size());
+		logger.debug("getAllSuppliers() - END");
+		return allSuppliersBeans;
 	}
 
 	public void registerSupplier(SupplierBean supplierBean) {
+		logger.debug("registerSupplier() - START");
+		logger.debug("registerSupplier() - supplierBean to Register " + supplierBean);
 		Optional<Boolean> isSupplierExists = supplierRepository.existsBySupplierNameAndSupplierCity(supplierBean.getSupplierName(), supplierBean.getSupplierCity());
-		if(isSupplierExists.isPresent() && isSupplierExists.get())
+		boolean bSupplierExists = isSupplierExists.isPresent() && isSupplierExists.get();
+		logger.debug("registerSupplier() - Supplier Exists " + bSupplierExists);
+		if(bSupplierExists) {
+			logger.debug("registerSupplier() - Supplier Exists, so, throwing SupplierExistsException");
 			throw new SupplierExistsException("this Supplier already exists");
+		}
 		SupplierEntity supplierEntity = SupplierEntity.builder()
 				.supplierName(supplierBean.getSupplierName())
 				.supplierCity(supplierBean.getSupplierCity())
 				.build();
+		logger.debug("registerSupplier() - supplierEntity to Register " + supplierEntity);
 		supplierRepository.save(supplierEntity);
+		logger.debug("registerSupplier() - Supplier Registered");
 	}
 
 }
